4d76c7a59fcba7a6bee3faea0bf695622f21e7d9
Chathurangi Edussuriya <edussuriyac@gmail.com>
2019-10-14 16:00:27 +0530

    modify get hisory of a batch

--- application/supplier/getHistoryBatch.js
+++ application/supplier/getHistoryBatch.js
@@ -49,8 +49,8 @@ async function main () {
         await gateway.connect(connectionProfile, connectionOptions);
         const network = await gateway.getNetwork('nckchannel');
         const contract = await network.getContract('nckcc');
-        const buyResponse = await contract.evaluateTransaction('getHistoryForBatch','46793579024');
-        console.log(`Transaction has been evaluated, result is: ${buyResponse}`);
+        const result = await contract.evaluateTransaction('getHistoryForBatch','46793579024');
+        console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
 
     } catch (error) {
 
