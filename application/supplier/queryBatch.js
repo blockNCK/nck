eab91353a00b4f5f4a85aa3e26fcf7e5d9561ec2
Chathurangi Edussuriya <edussuriyac@gmail.com>
2019-10-14 15:44:21 +0530

    add application

---
+++ application/supplier/queryBatch.js
@@ -0,0 +1,78 @@
+/*
+SPDX-License-Identifier: Apache-2.0
+*/
+
+/*
+ * This application has 6 basic steps:
+ * 1. Select an identity from a wallet
+ * 2. Connect to network gateway
+ * 3. Access PaperNet network
+ * 4. Construct request to issue commercial paper
+ * 5. Submit transaction
+ * 6. Process response
+ */
+
+'use strict';
+
+// Bring key classes into scope, most importantly Fabric SDK network class
+const fs = require('fs');
+const yaml = require('js-yaml');
+const { FileSystemWallet, Gateway } = require('fabric-network');
+
+// A wallet stores a collection of identities for use
+const wallet = new FileSystemWallet('../identity/user/adam/wallet');
+
+// Main program function
+async function main () {
+
+    // A gateway defines the peers used to access Fabric networks
+    const gateway = new Gateway();
+
+    // Main try/catch block
+    try {
+
+        // Specify userName for network access
+        const userName = 'Admin@supplier.nck.com';
+
+        // Load connection profile; will be used to locate a gateway
+        let connectionProfile = yaml.safeLoad(fs.readFileSync('./gateway/networkConnection.yaml', 'utf8'));
+
+        // Set connection options; identity and wallet
+        let connectionOptions = {
+            identity: userName,
+            wallet: wallet,
+            discovery: { enabled: false, asLocalhost: true }
+
+        };
+
+
+        await gateway.connect(connectionProfile, connectionOptions);
+        const network = await gateway.getNetwork('nckchannel');
+        const contract = await network.getContract('nckcc');
+        const buyResponse = await contract.submitTransaction('queryBatch', '46793579024');
+
+    } catch (error) {
+
+        console.log(`Error processing transaction. ${error}`);
+        console.log(error.stack);
+
+    } finally {
+
+        // Disconnect from the gateway
+        console.log('Disconnect from Fabric gateway.');
+        gateway.disconnect();
+
+    }
+}
+main().then(() => {
+
+    console.log('Create batch complete.');
+
+}).catch((e) => {
+
+    console.log('create program exception.');
+    console.log(e);
+    console.log(e.stack);
+    process.exit(-1);
+
+});
