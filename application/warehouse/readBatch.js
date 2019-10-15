16f3950f5a34bc85063d3caf01c6b4b89b2fbf16
Chathurangi Edussuriya <edussuriyac@gmail.com>
2019-10-15 07:24:56 +0530

    modify read batch

--- application/warehouse/readBatch.js
+++ application/warehouse/readBatch.js
@@ -39,7 +39,7 @@ async function main () {
         await gateway.connect(connectionProfile, connectionOptions);
         const network = await gateway.getNetwork('nckchannel');
         const contract = await network.getContract('nckcc');
-        const result = await contract.evaluateTransaction('getHistoryForBatch','8746296537');
+        const result = await contract.evaluateTransaction('getHistoryForBatch','46793579024');
         console.log(`Transaction has been evaluated, result is: ${result}`);
 
     } catch (error) {
