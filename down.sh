66b177cf48d985ffa9b4a2e06bbd28911f4d3ca8
Namila Bandara <namila007@gmail.com>
2020-07-26 10:39:45 +0000

    down

---
+++ down.sh
@@ -0,0 +1,13 @@
+echo "DELETING DOCKER CONTAINERS"
+docker container stop $(docker container ls -aq)
+docker container rm $(docker container ls -aq)
+docker container prune --force
+
+docker network prune --force
+docker volume prune --force
+
+# docker system prune -a --force
+echo 'DELETEING CRYPTO FILES'
+cd nck-network
+rm -rf channel-artifacts
+rm -rf crypto-configs
